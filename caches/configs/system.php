9a9cea7ccb87dc7e72a075f06b797b00512a2de9
bertGo <52950492+bertGo@users.noreply.github.com>
2020-08-08 15:52:31 +0800

    Update system.php

--- caches/configs/system.php
+++ caches/configs/system.php
@@ -10,7 +10,8 @@ return array(
 //Cookie配置
 'cookie_domain' => '', //Cookie 作用域
 'cookie_path' => '', //Cookie 作用路径
-'cookie_pre' => 'sHRqX_', //Cookie 前缀，同一域名下安装多套系统时，请修改Cookie前缀
+//'cookie_pre' => 'sHRqX_', //Cookie 前缀，同一域名下安装多套系统时，请修改Cookie前缀
+'cookie_pre' => 'zzdc_', 
 'cookie_ttl' => 0, //Cookie 生命周期，0 表示随浏览器进程
 //模板相关配置
 'tpl_root' => 'templates/', //模板保存物理路径
