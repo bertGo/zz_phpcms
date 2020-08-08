fed5faa7fc702b5c3bd47c3d985c400917894bc3
bertGo <52950492+bertGo@users.noreply.github.com>
2020-08-08 15:25:00 +0800

    Update database.php
    vim database.php

--- caches/configs/database.php
+++ caches/configs/database.php
@@ -2,11 +2,11 @@
 
 return array (
 	'default' => array (
-		'hostname' => 'localhost',
-		'port' => 3306,
-		'database' => 'phpcmsv9',
-		'username' => 'root',
-		'password' => '123456',
+		'hostname' => '123.59.106.86',
+		'port' => 25398,
+		'database' => 'tmp_zhongzhu',
+		'username' => 'uu_root',
+		'password' => 'xMdsrs3-3w0b303285Hsey37',
 		'tablepre' => 'v9_',
 		'charset' => 'utf8',
 		'type' => 'mysqli',
